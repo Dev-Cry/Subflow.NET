1b969653bbff5721531a9c058fef8942c047b20e
Tomáš Vaněk <tomas.vanek@vancloud.cz>
2025-03-28 14:01:15 +0100

    úprava validačního frameworku

--- Subflow.NET/IO/Loader/Validation/ValidatorFactory.cs
+++ Subflow.NET/IO/Loader/Validation/ValidatorFactory.cs
@@ -12,38 +12,44 @@ namespace Subflow.NET.IO.Loader.Validation
     {
         private readonly ILoggerFactory _loggerFactory;
 
-        public ValidatorFactory(ILoggerFactory loggerFactory)
-        {
-            _loggerFactory = loggerFactory;
-        }
-
         public IValidator<string> CreatePathValidator()
         {
-            return new CompositeValidator<string>(new IValidator<string>[]
+            return new CompositeValidator<string>(new[]
             {
-            new Validator<string>(new IValidationRule<string>[]
-            {
-                new NotEmptyPathRule(_loggerFactory.CreateLogger<NotEmptyPathRule>()),
-                new AbsolutePathRule(_loggerFactory.CreateLogger<AbsolutePathRule>()),
-                new PathLengthRule(_loggerFactory.CreateLogger<PathLengthRule>(), 260),
-                new FileExistsRule(_loggerFactory.CreateLogger<FileExistsRule>())
-            })
-            });
+                CreatePathRuleValidator()
+    });
         }
 
         public IValidator<FileInfo> CreateFileValidator()
         {
-            return new CompositeValidator<FileInfo>(new IValidator<FileInfo>[]
+            return new CompositeValidator<FileInfo>(new[]
             {
-            new Validator<FileInfo>(new IValidationRule<FileInfo>[]
+                 CreateFileRuleValidator()
+    });
+        }
+
+        private IValidator<string> CreatePathRuleValidator()
+        {
+            return new Validator<string>(new IValidationRule<string>[]
             {
-                new NotDirectoryRule(_loggerFactory.CreateLogger<NotDirectoryRule>()),
-                new NotEmptyFileRule(_loggerFactory.CreateLogger<NotEmptyFileRule>()),
-                new ExtensionAllowedRule(_loggerFactory.CreateLogger<ExtensionAllowedRule>(), new[] { ".srt", ".vtt" }),
-                new MaxFileSizeRule(_loggerFactory.CreateLogger<MaxFileSizeRule>(), 100 * 1024 * 1024),
-                new FileReadableRule(_loggerFactory.CreateLogger<FileReadableRule>())
-            })
+        new NotEmptyPathRule(_loggerFactory.CreateLogger<NotEmptyPathRule>()),
+        new AbsolutePathRule(_loggerFactory.CreateLogger<AbsolutePathRule>()),
+        new PathLengthRule(_loggerFactory.CreateLogger<PathLengthRule>(), 260),
+        new FileExistsRule(_loggerFactory.CreateLogger<FileExistsRule>())
             });
         }
+
+        private IValidator<FileInfo> CreateFileRuleValidator()
+        {
+            return new Validator<FileInfo>(new IValidationRule<FileInfo>[]
+            {
+        new NotDirectoryRule(_loggerFactory.CreateLogger<NotDirectoryRule>()),
+        new NotEmptyFileRule(_loggerFactory.CreateLogger<NotEmptyFileRule>()),
+        new ExtensionAllowedRule(_loggerFactory.CreateLogger<ExtensionAllowedRule>(), new[] { ".srt", ".vtt" }),
+        new MaxFileSizeRule(_loggerFactory.CreateLogger<MaxFileSizeRule>(), 100 * 1024 * 1024),
+        new FileReadableRule(_loggerFactory.CreateLogger<FileReadableRule>())
+            });
+        }
+
     }
 }
